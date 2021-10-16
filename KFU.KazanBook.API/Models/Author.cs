5a3ecab15d2eca33b9f922570be149c30c5d91b8
Kenneth Gibson <scaphoidederps@gmail.com>
2021-10-16 09:10:32 +0300

    Add project files.

---
+++ KFU.KazanBook.API/Models/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KFU.KazanBook.API.Models
+{
+	public class Author
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+	}
+}
---
+++ KFU.KazanBook.API/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KFU.KazanBook.API.Models
+{
+	public class Book
+	{
+		public Guid Id { get; set; }
+		public Guid AuthorId { get; set; }
+		public string Name { get; set; }
+	}
+}
